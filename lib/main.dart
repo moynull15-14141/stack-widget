883abdfa0be73cf1568863a77256115250a0f6ef
MH-Pollob <cochipollob@gmail.com>
2022-09-10 16:43:24 +0600

    New padding,column use,country name use

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,21 +14,37 @@ class stk extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Stack(
-            alignment: Alignment(-0.5, -.2),
-            children: <Widget>[
-              Container(
-                height: 200.0,
-                width: 300.0,
-                color: Color.fromARGB(255, 18, 169, 74),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Center(
+              child: Column(
+                children: [
+                  Stack(
+                    alignment: Alignment(-0.5, -.2),
+                    children: <Widget>[
+                      Container(
+                        height: 200.0,
+                        width: 300.0,
+                        color: Color.fromARGB(255, 18, 169, 74),
+                      ),
+                      Positioned(
+                          child: CircleAvatar(
+                        radius: 50.0,
+                        backgroundColor: Color.fromARGB(255, 255, 62, 62),
+                      )),
+                    ],
+                  ),
+                  Text(
+                    'Bangladesh',
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ],
               ),
-              Positioned(
-                  child: CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Color.fromARGB(255, 255, 62, 62),
-              )),
-            ],
+            ),
           ),
         ),
       ),
